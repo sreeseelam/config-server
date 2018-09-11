86729df140b1c40fbd4b7b3b177bd22166487a79
Seelam, Sreenivasa R <sreenivasa.r.seelam@accenture.com>
2018-09-11 15:11:56 -0400

    Renamed to customer

--- src/main/java/com/seelam/learning/configserver/ConfigServerApplication.java
+++ src/main/java/com/seelam/learning/configserver/ConfigServerApplication.java
@@ -1,10 +1,14 @@
 package com.seelam.learning.configserver;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableConfigServer
 public class ConfigServerApplication {
 
